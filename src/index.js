5c2ca08e4bd2c2a876d0b372f57ef94ab15ceeb2
rogeriop <pinheiroxrogerio@gmail.com>
2025-04-08 16:32:45 -0300

    02 Gerando a lógica do projeto

--- src/index.js
+++ src/index.js
@@ -3,7 +3,42 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
-    console.log(texto);
+    quebraEmParagrafos(texto);
+    //verificaPalavrasDuplicadas(texto);
 })
+
+function limpaPalavras(palavra) {
+    return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+}
+
+function quebraEmParagrafos(texto) {
+    const paragrafos = texto.toLowerCase().split('\n');
+    const contagem = paragrafos.flatMap((paragrafo) => {
+        if (!paragrafo.trim()) return []; 
+        return verificaPalavrasDuplicadas(paragrafo);
+        
+    });
+
+    /*
+    const contagem = paragrafos
+        .filter((paragrafo) => paragrafo.trim())
+        .map((paragrafo) => {
+        return verificaPalavrasDuplicadas(paragrafo);
+    })
+    */
+    console.log(contagem);
+}
+
+function verificaPalavrasDuplicadas(texto) {
+    const listaPalavras = texto.split(' ');
+    const resultado = {};
+    listaPalavras.forEach(palavra => {
+        if (palavra.length >= 3) {
+            const palavraLimpa = limpaPalavras(palavra);
+            resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1
+        }
+    });
+    return resultado;
+}
 
 
